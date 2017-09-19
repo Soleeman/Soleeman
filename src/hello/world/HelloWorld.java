e5b054947ee431906c7eb193489204c8dece8f08
sohak9660 <sohak9660@7KXL6Y1.Student.UGDSB.ED>
2017-09-19 15:05:38 -0400

    I made it say Hello World Im a code God

--- src/hello/world/HelloWorld.java
+++ src/hello/world/HelloWorld.java
@@ -16,7 +16,8 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here System.out.println("HW");
+         System.out.println("HW");
     }
-    
+   
 }
